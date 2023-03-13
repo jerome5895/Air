ee840228838a92ff6a446b8c16ea544d004ae545
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-13 15:16:15 -0400

    Added air00.py

---
+++ air00.py
@@ -0,0 +1,39 @@
+import sys
+
+# Function to separate argument
+def separation(separateurs):
+    string_a_couper = sys.argv[1]
+    words = []
+    current_word = ""
+    for char in string_a_couper:
+        if char in separateurs:
+            if current_word:
+                words.append(current_word)
+                current_word = ''    
+        else:
+            current_word += char
+    if current_word:
+        words.append(current_word)
+    return words
+
+# Function to manage argument errors
+def errors():
+    if len(sys.argv) != 2:
+        print("Invalid input. Please provide one argument.")
+        sys.exit()
+
+# Function to print out without []
+def without_brackets(words):
+    for i in range(len(words)):
+        print((words[i]), end='\n')
+    print()
+
+# Convert globales variables
+separateurs = [' ', '\t', '\n']
+
+# Resolution
+errors()
+words = separation(separateurs)
+
+# Print out result
+without_brackets(words)

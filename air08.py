82ab2e39745c8f5e399997fc21068c4270f53239
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-21 10:28:58 -0400

    Modify sort function to have good result, air08.py

--- air08.py
+++ air08.py
@@ -5,27 +5,31 @@ def separate(argument):
     separator = "fusion"
     if separator in argument:
         index = argument.index(separator)
-        array1 = " ".join(argument[:index])
-        array2 = " ".join(argument[index+1:])
+        array1 = argument[:index]
+        array2 = argument[index+1:]
     else:
         print("Invalid input. No 'fusion' word provided.")
         sys.exit()
     return array1, array2
 
-# Function to concatenete two arrays
-def concatenete(array1, array2):
-    new_list = []
-    for list in [array1], [array2]:
-        for element in list:
-            new_list.append(element)
-    return new_list
+# Function to sort and concatenate two arrays
+def sorted_fusion(array1, array2):
+    new_array = array1 + array2
+    n = len(new_array)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if new_array[j] < new_array[min_index]:
+                min_index = j
+        new_array[i], new_array[min_index] = new_array[min_index], new_array[i]
+    return new_array
 
 # Convert globales variables
 argument = sys.argv[1:]
 
 # Resolution
 array1, array2 = separate(argument)
-new_list = concatenete(array1, array2)
+new_array = sorted_fusion(array1, array2)
 
 # Print out result
-print(f"{new_list}")
+print(new_array)

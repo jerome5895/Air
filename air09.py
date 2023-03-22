e1246a8764e9be6c5680693edc40230b6b4957cb
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-22 11:49:12 -0400

    Added correction to function, air09.py

--- air09.py
+++ air09.py
@@ -5,13 +5,12 @@ def to_left():
     argument = sys.argv[1:]
     new_string = ""
     changed_word = ""
-    for word in argument:
-        if word != argument[0]:
+    for index, word in enumerate(argument):
+        if index != 0:
             new_string += word + " "
         else:
             changed_word = word
-            if new_string == argument[1:]:
-                new_string += changed_word
+    new_string += changed_word
     return new_string
 
 new_string = to_left()

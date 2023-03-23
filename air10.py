d68062ec2fc015c1b1bd0f866574155c2821a97d
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-23 15:45:47 -0400

    Added index_error function, edited read_file function and added resolution, air10.py

--- air10.py
+++ air10.py
@@ -1,16 +1,28 @@
 import sys
 import os
 
-if len(sys.argv) < 2:
-    print("Usage: python " + sys.argv[0] + " FILENAME")
-    sys.exit()
+# Function to manage index error
+def index_error():
+    if len(sys.argv) < 2:
+        print("Invalid input. Usage: python " + sys.argv[0] + " FILENAME")
+        sys.exit()
 
+# Function to manage file error
+def read_file():
+    try:
+        with open(filename, 'r') as file:
+            data = file.read()    
+    except IOError:
+        print("Error reading file. Please verify the filename.")
+        sys.exit()
+    return filename, data
 
+# Call function
+index_error()
+
+# Resolution
 filename = sys.argv[1]
-try:
-    with open(filename, 'r') as file:
-        data = file.read()
-    print(data)
-except IOError as e:
-    print("Error reading file:", e)
-    sys.exit(1)
+filename, data = read_file()
+
+# Print out result
+print(data)

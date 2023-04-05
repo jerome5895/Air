56514e7bfcb1209129dda42dd0577be6ddb385a9
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-05 15:37:50 -0400

    Added function and comments, air14.py

--- air14.py
+++ air14.py
@@ -1,1 +1,7 @@
-print("J'ai termine l'epreuve de l'air et c'etait a la fois difficile et facile, j'ai toujours autant de motivations pour la suite.")
+# Function that displays the string
+def print_string():
+    string = "J'ai termine l'epreuve de l'air et c'etait a la fois difficile et facile, j'ai toujours autant de motivations pour la suite."
+    print(string)
+
+# Print out the result
+print_string()
